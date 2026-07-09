69965846151334bf7573452330decd7ff2de6ba4
aroderg <anothersoul1staken@gmail.com>
2026-07-09 19:17:50 +0300

    refactor towerInfo.lua

--- towerInfo.lua
+++ towerInfo.lua
@@ -1,110 +1,70 @@
 --- Draw the tower and enemy stats and misc. info while in a battle.
 function towerInfo_visual()
-    if not player.menu.upgrades then
-        --[[ Print out tower info ]]--
-        love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
-        love.graphics.rectangle("fill", 10, 950, 200, 70)
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.rectangle("line", 10, 950, 200, 70)
-        love.graphics.setLineWidth(2)
-        love.graphics.setLineStyle("smooth")
-        if player.tower.currentHealth > 0 then
-            love.graphics.setColor(0, 0.5, 0.2, 1)
-            love.graphics.rectangle("fill", 15, 990, (player.tower.currentHealth/player.tower.health) * 190, 25, 2, 2)
-        end
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.rectangle("line", 15, 990, 190, 25, 2, 2)
-        love.graphics.setFont(font_AfacadBold20)
-        love.graphics.printf(string.format("%s/%s", notations.convertToLetterNotation(player.tower.currentHealth, "precise"), notations.convertToLetterNotation(player.tower.health, "precise")), 10, 988, 200, "center")
-        love.graphics.draw(img_icon_player_attack, 15, 955)
-        love.graphics.setFont(font_Afacad20)
-        love.graphics.printf(string.format("%s", notations.convertToLetterNotation(player.tower.attackDamage, "precise")), 42, 952, 200, "left")
-        love.graphics.setLineWidth(1)
-        love.graphics.setLineStyle("rough")
+    -- Determine the Y offset based on whether the upgrades menu is open
+    local yOffset = player.menu.upgrades and 720 or 950
 
-        --[[ Print out enemy info ]]--
-        love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
-        love.graphics.rectangle("fill", 1690, 950, 220, 70)
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.rectangle("line", 1690, 950, 220, 70)
-        love.graphics.draw(img_icon_enemy_health, 1695, 955)
-        love.graphics.printf(string.format("%s", notations.convertToLetterNotation(enemyAttributes.health, "precise")), 1722, 952, 200, "left")
-        love.graphics.draw(img_icon_enemy_attack, 1695, 991)
-        love.graphics.printf(string.format("%s", notations.convertToLetterNotation(enemyAttributes.attackDamage, "precise")), 1722, 988, 200, "left")
-
-        --[[ Print out gameplay info ]]--
-        love.graphics.setFont(font_Afacad20)
-        love.graphics.printf(string.format("Wave %d (D%d)", gameplay.wave, gameplay.difficulty), 1755, 952, 150, "right")
-        love.graphics.setLineStyle("smooth")
-        love.graphics.setColor(0.2, 0.2, 0.2, 1)
-        love.graphics.ellipse("line", 1875, 996, 20, 20)
-        love.graphics.ellipse("fill", 1875, 996, 20, 20)
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.setLineStyle("smooth")
-        love.graphics.setLineWidth(1)
-        if timers.nextWave == 0 then
-            love.graphics.arc("line", 1875, 996, 20, 1.5*math.pi, 3.5*math.pi - ((player.stats.wave.enemiesKilled / enemyAttributes.waveCap) * 2 * math.pi))
-            love.graphics.arc("fill", 1875, 996, 20, 1.5*math.pi, 3.5*math.pi - ((player.stats.wave.enemiesKilled / enemyAttributes.waveCap) * 2 * math.pi))
-        else
-            love.graphics.setColor(1, 1, 1, 1)
-            love.graphics.arc("line", 1875, 996, 20, 1.5*math.pi, 3.5*math.pi - (timers.nextWave / gameplay.waveCooldown) * 2 * math.pi)
-            love.graphics.arc("fill", 1875, 996, 20, 1.5*math.pi, 3.5*math.pi - (timers.nextWave / gameplay.waveCooldown) * 2 * math.pi)
-        end
-        love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
-        love.graphics.setLineWidth(10)
-        love.graphics.ellipse("line", 1875, 996, 5, 5)
-        
-    else
-        --[[ Print out tower info ]]--
-        love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
-        love.graphics.rectangle("fill", 10, 720, 200, 70)
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.rectangle("line", 10, 720, 200, 70)
-        love.graphics.setLineWidth(2)
-        love.graphics.setLineStyle("smooth")
-        if player.tower.currentHealth > 0 then
-            love.graphics.setColor(0, 0.5, 0.2, 1)
-            love.graphics.rectangle("fill", 15, 760, (player.tower.currentHealth/player.tower.health) * 190, 25, 2, 2)
-        end
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.rectangle("line", 15, 760, 190, 25, 2, 2)
-        love.graphics.setFont(font_AfacadBold20)
-        love.graphics.printf(string.format("%s/%s", notations.convertToLetterNotation(player.tower.currentHealth, "precise"), notations.convertToLetterNotation(player.tower.health, "precise")), 10, 758, 200, "center")
-        love.graphics.draw(img_icon_player_attack, 15, 725)
-        love.graphics.setFont(font_Afacad20)
-        love.graphics.printf(string.format("%s", notations.convertToLetterNotation(player.tower.attackDamage, "precise")), 42, 722, 200, "left")
-        love.graphics.setLineWidth(1)
-        love.graphics.setLineStyle("rough")
-
-        --[[ Print out enemy info ]]--
-        love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
-        love.graphics.rectangle("fill", 1690, 720, 220, 70)
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.rectangle("line", 1690, 720, 220, 70)
-        love.graphics.draw(img_icon_enemy_health, 1695, 725)
-        love.graphics.printf(string.format("%s", notations.convertToLetterNotation(enemyAttributes.health, "precise")), 1722, 722, 200, "left")
-        love.graphics.draw(img_icon_enemy_attack, 1695, 761)
-        love.graphics.printf(string.format("%s", notations.convertToLetterNotation(enemyAttributes.attackDamage, "precise")), 1722, 758, 200, "left")
-
-        --[[ Print out gameplay info ]]--]
-        love.graphics.printf(string.format("Wave %d (D%d)", gameplay.wave, gameplay.difficulty), 1755, 722, 150, "right")
-        love.graphics.setLineStyle("smooth")
-        love.graphics.setColor(0.2, 0.2, 0.2, 1)
-        love.graphics.ellipse("line", 1875, 766, 20, 20)
-        love.graphics.ellipse("fill", 1875, 766, 20, 20)
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.setLineStyle("smooth")
-        love.graphics.setLineWidth(1)
-        if timers.nextWave == 0 then
-            love.graphics.arc("line", 1875, 766, 20, 1.5*math.pi, 3.5*math.pi - ((player.stats.wave.enemiesKilled / enemyAttributes.waveCap) * 2 * math.pi))
-            love.graphics.arc("fill", 1875, 766, 20, 1.5*math.pi, 3.5*math.pi - ((player.stats.wave.enemiesKilled / enemyAttributes.waveCap) * 2 * math.pi))
-        else
-            love.graphics.setColor(1, 1, 1, 1)
-            love.graphics.arc("line", 1875, 766, 20, 1.5*math.pi, 3.5*math.pi - (timers.nextWave / gameplay.waveCooldown) * 2 * math.pi)
-            love.graphics.arc("fill", 1875, 766, 20, 1.5*math.pi, 3.5*math.pi - (timers.nextWave / gameplay.waveCooldown) * 2 * math.pi)
-        end
-        love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
-        love.graphics.setLineWidth(10)
-        love.graphics.ellipse("line", 1875, 766, 5, 5)
+    --[[ Print out tower info ]]--
+    love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
+    love.graphics.rectangle("fill", 10, yOffset, 200, 70)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.rectangle("line", 10, yOffset, 200, 70)
+    love.graphics.setLineWidth(2)
+    love.graphics.setLineStyle("smooth")
+    
+    if player.tower.currentHealth > 0 then
+        love.graphics.setColor(0, 0.5, 0.2, 1)
+        love.graphics.rectangle("fill", 15, yOffset + 40, (player.tower.currentHealth / player.tower.health) * 190, 25, 2, 2)
     end
+    
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.rectangle("line", 15, yOffset + 40, 190, 25, 2, 2)
+    love.graphics.setFont(font_AfacadBold20)
+    love.graphics.printf(string.format("%s/%s", notations.convertToLetterNotation(player.tower.currentHealth, "precise"), notations.convertToLetterNotation(player.tower.health, "precise")), 10, yOffset + 38, 200, "center")
+    
+    love.graphics.draw(img_icon_player_attack, 15, yOffset + 5)
+    love.graphics.setFont(font_Afacad20)
+    love.graphics.printf(string.format("%s", notations.convertToLetterNotation(player.tower.attackDamage, "precise")), 42, yOffset + 2, 200, "left")
+    love.graphics.setLineWidth(1)
+    love.graphics.setLineStyle("rough")
+
+    --[[ Print out enemy info ]]--
+    love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
+    love.graphics.rectangle("fill", 1690, yOffset, 220, 70)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.rectangle("line", 1690, yOffset, 220, 70)
+    
+    love.graphics.draw(img_icon_enemy_health, 1695, yOffset + 5)
+    love.graphics.printf(string.format("%s", notations.convertToLetterNotation(enemyAttributes.health, "precise")), 1722, yOffset + 2, 200, "left")
+    
+    love.graphics.draw(img_icon_enemy_attack, 1695, yOffset + 41)
+    love.graphics.printf(string.format("%s", notations.convertToLetterNotation(enemyAttributes.attackDamage, "precise")), 1722, yOffset + 38, 200, "left")
+
+    --[[ Print out gameplay info ]]--
+    love.graphics.printf(string.format("Wave %d (D%d)", gameplay.wave, gameplay.difficulty), 1755, yOffset + 2, 150, "right")
+    love.graphics.setLineStyle("smooth")
+    love.graphics.setColor(0.2, 0.2, 0.2, 1)
+    love.graphics.ellipse("line", 1875, yOffset + 46, 20, 20)
+    love.graphics.ellipse("fill", 1875, yOffset + 46, 20, 20)
+    
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setLineStyle("smooth")
+    love.graphics.setLineWidth(1)
+    
+    -- Calculate arc percentage
+    local arcPercent = 0
+    if timers.nextWave == 0 then
+        arcPercent = player.stats.wave.enemiesKilled / enemyAttributes.waveCap
+    else
+        arcPercent = timers.nextWave / gameplay.waveCooldown
+    end
+    
+    local startAngle = 1.5 * math.pi
+    local endAngle = 3.5 * math.pi - (arcPercent * 2 * math.pi)
+    
+    love.graphics.arc("line", 1875, yOffset + 46, 20, startAngle, endAngle)
+    love.graphics.arc("fill", 1875, yOffset + 46, 20, startAngle, endAngle)
+    
+    love.graphics.setColor(accentColors[player.misc.theme].towerInfo)
+    love.graphics.setLineWidth(10)
+    love.graphics.ellipse("line", 1875, yOffset + 46, 5, 5)
 end
